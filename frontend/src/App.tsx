6e529aac68fb03165cb667264854495c201a339a
Paulo Carneiro <paulocarneirosegundo@gmail.com>
2022-09-06 03:01:11 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Hello World</h1>
+  return (
+    <>
+      <h1>Hello World</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
+
   )
 }
 
