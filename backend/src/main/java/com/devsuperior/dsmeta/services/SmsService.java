3eb08df68d75ad932ec6bb5578846d77013ba626
Paulo Carneiro <paulocarneirosegundo@gmail.com>
2022-09-10 22:19:52 -0300

    Axios, useEffect first request

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -34,8 +34,9 @@ public class SmsService {
 		
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
-		String msg = "O vendedor " + sale.getSellerName() + "foi destaque em " + date 
-				+ "com um total de R$ " + String.format("%.2f", sale.getAmount());
+		String msg = String.format("O vendedor %s foi destaque em %s com um total de R$ %.2f", 
+				sale.getSellerName(), 
+				date, sale.getAmount());
 
 		Twilio.init(twilioSid, twilioKey);
 
--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -10,7 +10,7 @@ function Header() {
                     <img src={logo} alt="DSmeta" />
                         <h1>DSMeta</h1>
                         <p>Desenvolvido por
-                            <a href="github.com/Paulocarneiroo">Paulocarneiroo</a>
+                            <a href="github.com/Paulocarneiroo"> Paulocarneiroo</a>
                         </p>
                 </div>
             </header>
